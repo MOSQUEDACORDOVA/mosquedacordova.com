5a7255cfccc2c2397ee85b0803bc6a98d193475e
MOSQUEDACORDOVA <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2023-02-11 11:15:36 -0500

    deteccion de idioma corregido

--- public/assets/js/translate.js
+++ public/assets/js/translate.js
@@ -1,13 +1,16 @@
 window.onload = function () {
- //alert(window.location);
+
 	let ln = navigator.language || navigator.userLanguage;
-	let url= 'index.html'
-	if (ln == 'es-419' || ln == 'es-US' || ln == 'es' || ln == 'es') {
-	  console.log("español")
-	  window.location.replace(url);
-	}else{
-		url= 'indexEN.html' //INGLES
-	 	console.log("cualquier otro idioma:", ln)
+	let url= 'index.html';
+
+	if (ln == 'es-419' || ln == 'es-US' || ln == 'es' || ln == 'es' || ln == 'es-ES') {
+	  
+	 	console.log("Idioma:", ln);
+	}
+	else {
+	  
+		url= 'indexEN.html'; //INGLES
+	 	console.log("cualquier otro idioma:", ln);
 	 	window.location.replace(url);
 	}
  }
